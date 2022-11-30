15cc18726d71c753a016c1960cf9fa793678e767
Travis Cunningham <tcunningham@qumulo.com>
2022-11-30 10:34:31 -0800

    Add VPC terraform templates for testing

--- tests/test_terraform.py
+++ tests/test_terraform.py
@@ -1,6 +1,19 @@
 import unittest
+import uuid
+
+from tests.utils import TerraformExecutor, TerraformLogLevel
 
 
-class TestCase(unittest.TestCase):
-    def test_simple(self):
-        self.assertEqual(1, 1)
+class TestDeployVPC(unittest.TestCase):
+    def test_happy_path(self):
+        executor = TerraformExecutor(
+            terraform_workspace='test',
+            terraform_vars_file='terraform.tfvars',
+            terraform_vars={"execution_id": str(uuid.uuid4())},
+            module_path='tests/vpc-terraform',
+            log_level=TerraformLogLevel.INFO,
+        )
+        try:
+            executor.deploy()
+        finally:
+            executor.destroy()
---
+++ tests/vpc-terraform/vars.tf
@@ -0,0 +1,4 @@
+variable "execution_id" {}
+variable "main_vpc_cidr" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
